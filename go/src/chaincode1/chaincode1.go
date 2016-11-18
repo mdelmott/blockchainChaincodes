2a29f052b267fd66f77c0dffbf2c8f538d62d14e
mdelmott <delmottemartin@gmail.com>
2016-11-18 15:48:11 +0100

    add table functions

--- go/src/chaincode1/chaincode1.go
+++ go/src/chaincode1/chaincode1.go
@@ -34,42 +34,6 @@ import (
 type SimpleChaincode struct {
 }
 
-/*func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 4")
-	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return nil, err
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
-}*/
-
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	var A string    // Entities
 	var Aval int // Asset holdings
@@ -91,23 +55,78 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 		}
 
 	}
+
+	err = t.createTable(stub)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating table one during init. %s", err)
+	}
+
 	return nil, nil
+}
+
+func (t *SimpleChaincode) createTable(stub *shim.ChaincodeStub) error {
+
+	var columnDefsTable []*shim.ColumnDefinition
+	columnOneTableDef := shim.ColumnDefinition{Name: "colOne",
+		Type: shim.ColumnDefinition_STRING, Key: true}
+	columnTwoTableDef := shim.ColumnDefinition{Name: "colTwo",
+		Type: shim.ColumnDefinition_STRING, Key: false}
+	columnThreeTableDef := shim.ColumnDefinition{Name: "colThree",
+		Type: shim.ColumnDefinition_STRING, Key: false}
+	columnFourTableDef := shim.ColumnDefinition{Name: "colFour",
+		Type: shim.ColumnDefinition_STRING, Key: false}
+	columnDefsTable = append(columnDefsTable, &columnOneTableDef)
+	columnDefsTable = append(columnDefsTable, &columnTwoTableDef)
+	columnDefsTable = append(columnDefsTable, &columnThreeTableDef)
+	columnDefsTable = append(columnDefsTable, &columnFourTableDef)
+	return stub.CreateTable("table", columnDefsTable)
 }
 
 // Transaction makes payment of X units from A to B
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 
-	function = args[0];
+	function = args[0]
 
 	switch function {
 	case "add" : return t.add(stub, args)
 	case "delete" : return t.delete(stub, args)
 	case "transaction" : return t.transaction(stub, args)
 	case "fusion" : return t.fusion(stub, args)
+	case "insertTable" : return t.insertTable(stub, args)
 	default: return nil, errors.New("Incorrect name of function")
 	}
 }
 
+func (t *SimpleChaincode) insertTable(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	var attr1, attr2, attr3, attr4;
+
+	attr1 = "table"
+	attr2 = args[1]
+	attr3 = args[2]
+	attr4 = args[3]
+
+	var columns []*shim.Column
+	col1 := shim.Column{Value: &shim.Column_String_{String_: attr1}}
+	col2 := shim.Column{Value: &shim.Column_String_{String_: attr2}}
+	col3 := shim.Column{Value: &shim.Column_String_{String_: attr3}}
+	col4 := shim.Column{Value: &shim.Column_String_{String_: attr4}}
+	columns = append(columns, &col1)
+	columns = append(columns, &col2)
+	columns = append(columns, &col3)
+	columns = append(columns, &col4)
+
+	row := shim.Row{Columns: columns}
+	ok, err := stub.InsertRow("table", row)
+	if err != nil {
+		return nil, fmt.Errorf("insertRowTable operation failed. %s", err)
+	}
+	if !ok {
+		return nil, errors.New("insertRowTable operation failed. Row with given key already exists")
+	}
+
+	return nil, nil
+}
 
 func (t *SimpleChaincode) add(stub *shim.ChaincodeStub, args []string) ([]byte, error){
 	var A string    // Entities
@@ -245,17 +264,22 @@ func (t *SimpleChaincode) delete(stub *shim.ChaincodeStub, args []string) ([]byt
 
 // Query callback representing the query of a chaincode
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	//if function != "query" {
-	//	return nil, errors.New("Invalid query function name. Expecting \"query\"")
-	//}
+
+	function = args[0];
+
+	switch function {
+	case "queryElement" : return t.queryElement(stub, args)
+	case "queryTable" : return t.queryTable(stub, args)
+	default: return nil, errors.New("Incorrect name of function")
+	}
+
+}
+
+func (t *SimpleChaincode) queryElement(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var A string // Entities
 	var err error
 
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
-	}
-
-	A = args[0]
+	A = args[1]
 
 	// Get the state from the ledger
 	Avalbytes, err := stub.GetState(A)
@@ -274,35 +298,41 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	return Avalbytes, nil
 }
 
-// Query callback representing the query of a chaincode
-/*func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	if function != "query" {
-		return nil, errors.New("Invalid query function name. Expecting \"query\"")
-	}
-	var A string // Entities
-	var values [][]byte
+func (t *SimpleChaincode) queryTable(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 
-	for i := 0; i<len(args); i++{
+	var columns []shim.Column
 
-		var err error
+	col1Val := "table"
+	col1 := shim.Column{Value: &shim.Column_String_{String_: col1Val}}
+	columns = append(columns, col1)
 
-		A = args [i]
-		// Get the state from the ledger
-		Avalbytes, err := stub.GetState(A)
-
-		if err != nil {
-			jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-			return nil, errors.New(jsonResp)
-		}
-		if Avalbytes == nil {
-			jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-			return nil, errors.New(jsonResp)
-		}
+	rowChannel, err := stub.GetRows("tableTwo", columns)
+	if err != nil {
+		return nil, fmt.Errorf("getRowsTableTwo operation failed. %s", err)
 	}
 
-	return values, nil
-}*/
+	var rows []shim.Row
+	for {
+		select {
+		case row, ok := <-rowChannel:
+			if !ok {
+				rowChannel = nil
+			} else {
+				rows = append(rows, row)
+			}
+		}
+		if rowChannel == nil {
+			break
+		}
+	}
 
+	jsonRows, err := json.Marshal(rows)
+	if err != nil {
+		return nil, fmt.Errorf("getRowsTableTwo operation failed. Error marshaling JSON: %s", err)
+	}
+
+	return jsonRows, nil
+}
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
